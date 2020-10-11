abcf9f64a3a6482ffbc5e8fbb68483b8e5537593
Jeon <wmf85276@gmail.com>
2020-10-12 00:27:58 +0900

    BtnSettingActivity: 버튼 수량, Next 버튼 기능

--- app/src/main/java/com/hun/cnk_remote_controller/BtnSettingActivity.kt
+++ app/src/main/java/com/hun/cnk_remote_controller/BtnSettingActivity.kt
@@ -2,7 +2,11 @@ package com.hun.cnk_remote_controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_setting_button.*
 import kotlinx.android.synthetic.main.activity_setting_main.*
 
 class BtnSettingActivity : AppCompatActivity() {
@@ -11,6 +15,22 @@ class BtnSettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_button)
 
+        var inputBtnCount: Int = 0
 
+        edit_button_count.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                inputBtnCount = edit_button_count.text.toString().toInt()
+            }
+        })
+
+        btn_next.setOnClickListener {
+            val intent = Intent(this, BtnModifyActivity::class.java)
+            intent.putExtra("btn_count", inputBtnCount)
+            startActivity(intent)
+        }
     }
 }
