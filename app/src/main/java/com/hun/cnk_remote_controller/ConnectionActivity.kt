4c9811aa46fcb49e2b2753de672095b94418fba7
Jeon <wmf85276@gmail.com>
2020-10-16 14:40:47 +0900

    Kotlin: ConnectionActivity

--- app/src/main/java/com/hun/cnk_remote_controller/ConnectionActivity.kt
+++ app/src/main/java/com/hun/cnk_remote_controller/ConnectionActivity.kt
@@ -2,11 +2,32 @@ package com.hun.cnk_remote_controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.hun.cnk_remote_controller.adapter.ConnBtnAdapter
+import com.hun.cnk_remote_controller.data.BtnRealmObject
+import com.hun.cnk_remote_controller.data.ButtonItem
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_connection.*
 
 class ConnectionActivity : AppCompatActivity() {
+
+    private val buttonItems: ArrayList<ButtonItem> = ArrayList()
+    private val connBtnAdapter = ConnBtnAdapter(buttonItems)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connection)
+
+        recycler_button_list.adapter = connBtnAdapter
+        recycler_button_list.layoutManager = GridLayoutManager(this, 2)
+        listBtnItems()
+    }
+
+    private fun listBtnItems() {
+        val btnResults = Realm.getDefaultInstance().where(BtnRealmObject::class.java).findAll()
+
+        for (btnResult in btnResults) {
+            connBtnAdapter.addItem(btnResult.name)
+        }
     }
 }
