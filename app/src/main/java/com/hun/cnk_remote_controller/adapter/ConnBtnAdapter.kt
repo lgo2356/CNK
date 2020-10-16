530a5fe046fbf061cec494a2f18134d4d1bd3d40
Jeon <wmf85276@gmail.com>
2020-10-16 17:48:22 +0900

    Kotlin: ItemTouchListener 추가

--- app/src/main/java/com/hun/cnk_remote_controller/adapter/ConnBtnAdapter.kt
+++ app/src/main/java/com/hun/cnk_remote_controller/adapter/ConnBtnAdapter.kt
@@ -1,6 +1,9 @@
 package com.hun.cnk_remote_controller.adapter
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -11,7 +14,19 @@ import com.hun.cnk_remote_controller.data.ButtonItem
 class ConnBtnAdapter(private val items: ArrayList<ButtonItem>) :
     RecyclerView.Adapter<ConnBtnAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConnBtnAdapter.ViewHolder {
+    private var itemTouchListener: OnItemTouchListener? = null
+
+    interface OnItemTouchListener {
+        fun onItemTouchActionDown(view: View, motionEvent: MotionEvent, position: Int)
+        fun onItemTouchActionUp(view: View, motionEvent: MotionEvent, position: Int)
+        fun onItemTouchActionCancel(view: View, motionEvent: MotionEvent, position: Int)
+    }
+
+    fun setOnItemTouchListener(listener: OnItemTouchListener) {
+        this.itemTouchListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.item_conn_button, parent, false)
         return ViewHolder(view)
@@ -19,6 +34,28 @@ class ConnBtnAdapter(private val items: ArrayList<ButtonItem>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.tvBtnName.text = items[position].name
+
+        holder.itemView.setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d("Debug", "Action down")
+                    itemTouchListener?.onItemTouchActionDown(view, motionEvent, position)
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    Log.d("Debug", "Action up")
+                    itemTouchListener?.onItemTouchActionUp(view, motionEvent, position)
+                    view.performClick()
+                }
+
+                MotionEvent.ACTION_CANCEL -> {
+                    Log.d("Debug", "Action cancel")
+                    itemTouchListener?.onItemTouchActionCancel(view, motionEvent, position)
+                    view.performClick()
+                }
+            }
+            true
+        }
     }
 
     override fun getItemCount(): Int {
