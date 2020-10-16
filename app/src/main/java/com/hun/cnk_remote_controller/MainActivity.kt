9e07f546ee4ff5fdf44014f59f3f041a222c69da
Jeon <wmf85276@gmail.com>
2020-10-16 15:00:46 +0900

    MainActivity tool bar 추가

--- app/src/main/java/com/hun/cnk_remote_controller/MainActivity.kt
+++ app/src/main/java/com/hun/cnk_remote_controller/MainActivity.kt
@@ -3,12 +3,16 @@ package com.hun.cnk_remote_controller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar_main)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         btn_connection.setOnClickListener {
             val intent = Intent(this, ConnectionActivity::class.java)
@@ -18,6 +22,29 @@ class MainActivity : AppCompatActivity() {
         btn_setting.setOnClickListener {
             val intent = Intent(this, SettingActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_bluetooth_setting -> {
+                bluetoothDialog = BluetoothDialogFragment(handler)
+
+                if (::bluetoothDialog.isInitialized) {
+                    bluetoothDialog.show(supportFragmentManager, "missiles")
+                }
+
+                true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
         }
     }
 }
