70f4abc5d9468fee43abf18491519db6713c8214
Jeon <wmf85276@gmail.com>
2020-10-21 05:12:48 +0900

    Kotlin: 버튼 등록 제한 기능 추가

--- app/src/main/java/com/hun/cnk_remote_controller/BtnSettingActivity.kt
+++ app/src/main/java/com/hun/cnk_remote_controller/BtnSettingActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
+import com.hun.cnk_remote_controller.data.BtnRealmObject
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_setting_button.*
 import kotlinx.android.synthetic.main.activity_setting_main.*
+import java.lang.Exception
 
 class BtnSettingActivity : AppCompatActivity() {
 
@@ -15,7 +20,7 @@ class BtnSettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting_button)
 
-        var inputBtnCount: Int = 0
+        var inputBtnCount = 0
 
         edit_button_count.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {}
@@ -23,17 +28,36 @@ class BtnSettingActivity : AppCompatActivity() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                inputBtnCount = edit_button_count.text.toString().toInt()
+                inputBtnCount = try {
+                    edit_button_count.text.toString().toInt()
+                } catch (e: Exception) {
+                    0
+                }
             }
         })
 
         btn_next.setOnClickListener {
+            val btnCount: Int = Realm.getDefaultInstance().where(BtnRealmObject::class.java).findAll().count()
+            if (btnCount + inputBtnCount > 10) {
+//                showSnackBar("최대 10개까지 입력할 수 있습니다. (현재: ${btnCount}개)")
+                Toast.makeText(this, "최대 10개까지 입력할 수 있습니다. (현재: ${btnCount}개)", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (inputBtnCount == 0) {
+                return@setOnClickListener
+            }
+
             val intent = Intent(this, BtnModifyActivity::class.java)
             intent.putExtra("req_code", Constant.REQ_NEW_BTN)
             intent.putExtra("btn_count", inputBtnCount)
             startActivityForResult(intent, Constant.REQ_NEW_BTN)
-//            startActivity(intent)
         }
+    }
+
+    private fun showSnackBar(message: String) {
+        val snackBar = Snackbar.make(main_container, message, Snackbar.LENGTH_INDEFINITE)
+        snackBar.setAction("확인") { snackBar.dismiss() }.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
