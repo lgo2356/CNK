e9c10e6e3800db4d7ec2d17a4a62fede40fea787
Jeon <wmf85276@gmail.com>
2020-11-01 16:29:45 +0900

    kt: button state 추가

--- app/src/main/java/com/hun/cnk_remote_controller/data/ButtonItem.kt
+++ app/src/main/java/com/hun/cnk_remote_controller/data/ButtonItem.kt
@@ -4,4 +4,5 @@ class ButtonItem {
     var number: Int = -1
     var name: String = "UNDEFINED"
     var mode: Boolean = false  // false: 수동, true: 자동
+    var state: Boolean = false // true: 눌림, false: 안 눌림
 }
